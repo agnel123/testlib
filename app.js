5c90b4d757ef955a3774bfd7d17fc4ecc2518ff8
Agnel <agnel.123@gmail.com>
2018-05-03 19:19:34 -0400

    adding send file

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var port = process.env.PORT || 8080;
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.send('helloworld');
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, function() {

08287c998e229e710b8ced8e8f32899a5b79647f
Agnel <agnel.123@gmail.com>
2018-05-03 19:15:49 -0400

    updated

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+
+// make express look in the public directory for assets (css/js/img)
+
+// set the home page route
 app.get('/', function(req, res) {
-   res.send('helloworld');
+
+	// ejs render automatically looks in the views folder
+	res.send('helloworld');
 });
 
-app.listen(8080);
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
